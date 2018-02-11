94ad0bbfb360f91b6497018ebd2e02ebb413409e
tommygraven <tommygraven@me.com>
2018-02-11 14:44:40 +0100

    Applied layer to remote and local players

---
+++ Assets/PlayerShoot.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerShoot : NetworkBehaviour {
+	public PlayerWeapon weapon;
+
+	[SerializeField] Camera cam;
+	[SerializeField] LayerMask mask;
+
+	// Use this for initialization
+	void Start () {
+		if (cam == null)
+		{
+			Debug.LogError("PlayerShoot: No camera referenced!");
+			this.enabled = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1"))
+		{
+			Shoot();
+		}
+	}
+
+	void Shoot()
+	{
+		RaycastHit _hit;
+		if (Physics.Raycast(cam.transform.position, cam.transform.forward, out _hit, weapon.range, mask))
+		{
+			Debug.Log("We hit " + _hit.collider.name);
+		}
+	}
+}
--- Assets/Scripts/PlayerSetup.cs
+++ Assets/Scripts/PlayerSetup.cs
@@ -6,25 +6,40 @@ using UnityEngine.Networking;
 public class PlayerSetup : NetworkBehaviour {
 
 	[SerializeField] Behaviour[] componentsToDisable;
+	[SerializeField] string localLayerName = "LocalPlayer";
+	[SerializeField] string remoteLayerName = "RemotePlayer";
+
 	Camera sceneCamera;
 
 	void Start()
 	{
 		if (!isLocalPlayer)
 		{
-			for(int i = 0; i < componentsToDisable.Length; i++)
-			{
-				componentsToDisable[i].enabled = false;
-			}
+			DisableComponents();
+			AssignRemoteLayer();
 		}
 
 		else {
+			gameObject.layer = LayerMask.NameToLayer(localLayerName);
 			sceneCamera = Camera.main;
 			if (sceneCamera != null)
 			{
 				sceneCamera.gameObject.SetActive(false);
 			}
 
+		}
+	}
+
+	void AssignRemoteLayer()
+	{
+		gameObject.layer = LayerMask.NameToLayer(remoteLayerName);
+	}
+
+	void DisableComponents()
+	{
+		for(int i = 0; i < componentsToDisable.Length; i++)
+		{
+			componentsToDisable[i].enabled = false;
 		}
 	}
 
